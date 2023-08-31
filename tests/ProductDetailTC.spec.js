a9bf6e630aac0cddac3f9a0311ca94b166219942
Binh Ha <61079709+hvbinh@users.noreply.github.com>
2023-08-31 17:23:07 +0700

    Update ProductDetailTC.spec.js

--- tests/ProductDetailTC.spec.js
+++ tests/ProductDetailTC.spec.js
@@ -27,6 +27,7 @@ test('Verify that product detail page displays correctly',async ({page})=>{
     logger.info('Verify product detail displays correctly');
     await expect(await productDetail.getProductTitle()).toBe('Sauce Labs Backpack');
     console.log('test');
+    console.log('test1');
 
 
 })
