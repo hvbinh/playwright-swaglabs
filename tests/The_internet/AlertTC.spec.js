d47475e277b334b952ec96bb34eaa0b588c1fbe4
hvbinh <havanbinhk13t01@gmail.com>
2023-09-21 23:57:55 +0700

    remove comment

--- tests/The_internet/AlertTC.spec.js
+++ tests/The_internet/AlertTC.spec.js
@@ -6,15 +6,6 @@ test('Verify alert', async ({ page }) => {
     await page.locator("//button[text()='Click for JS Confirm']").click();
     await page.waitForTimeout(3000);
 
-
-    // page.on('dialog', dialog =>{
-    //     //console.log(dialog.message());
-    //      dialog.accept();
-    // });
-
-    // expect(await page.locator("//p[@id='result']").innerText()).toBe("You clicked: Ok");
-    // await page.waitForTimeout(3000);
-
     page.once('dialog', dialog => {
         console.log(`Dialog message: ${dialog.message()}`);
         //dialog.accept('').catch(() => { });
