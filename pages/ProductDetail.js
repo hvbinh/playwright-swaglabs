b16314957d2f19852220f50ace79e8f4a1866e0e
hvbinh <havanbinhk13t01@gmail.com>
2023-10-17 16:21:33 +0700

    ad tc "Verify that can click to Add to cart button"

--- pages/ProductDetail.js
+++ pages/ProductDetail.js
@@ -6,7 +6,10 @@ export class ProductDetail{
         this.product_description = page.locator("//div[contains(@class,'inventory_details_desc large_size')]");
         this.product_price = page.locator("//div[contains(@class,'inventory_details_price')]");
         this.add_to_cart_button = page.locator("//button[text()='Add to cart']");
+        this.remove_button = page.locator("//button[@name='remove-sauce-labs-backpack']");
         this.shopping_cart_badge = page.locator("//span[@class='shopping_cart_badge']");
+        this.shopping_cart_empty = page.locator("//a[@class='shopping_cart_link']");
+        
     }
     getProductTitle()
     {
@@ -28,4 +31,17 @@ export class ProductDetail{
     {
         return this.product_price.innerText();
     }
+    getShoppingCartBadgeNumber()
+    {
+        return this.shopping_cart_badge.innerText();
+    }
+    async clickToRemoveButton()
+    {
+        await this.remove_button.click();
+    }
+    getShoppingCartEmpty()
+    {
+        return this.shopping_cart_empty.innerText();
+    }
+
 }
--- tests/ProductDetailTC.spec.js
+++ tests/ProductDetailTC.spec.js
@@ -40,7 +40,44 @@ test('Verify that product detail page displays correctly',async ({page})=>{
 
     logger.info('Verify product price');
     await expect(await productDetail.getProductPrice()).toBe(price);
+ 
+    
+})
+test('Verify that can click to Add to cart button',async ({page})=>{
+    const login = PageGenerator.getLoginPage(page);
+    
+    logger.info('Go to https://www.saucedemo.com/');
+    await login.gotoURL(GlobalConstant.URL);
 
-    
-    
+    logger.info('Input to username textbox');
+    await login.inputToUsernameTextbox(GlobalConstant.STANDARD_USER);
+
+    logger.info('Input to password textbox');
+    await login.inputToPasswordTextbox(GlobalConstant.PASSWORD);
+
+    logger.info('Click to login button');
+    await login.clickToLoginButton();
+
+    const home = PageGenerator.getHomePage(page);
+    logger.info("Click to Add to cart button");
+    home.clickToAddToCartButton();
+
+    logger.info('Verify shopping cart badge is 1');
+    await expect(await home.getShoppingCartBadgeNumber()).toBe("1");
+
+    logger.info('Click to first product');
+    await home.clickToFirstProduct();
+    const productDetail = PageGenerator.getProductDetailPage(page);
+
+    logger.info('Verify shopping cart badge is 1');
+    await expect(await productDetail.getShoppingCartBadgeNumber()).toBe("1");
+
+    logger.info('Click to remove button');
+    await productDetail.clickToRemoveButton();
+
+    logger.info('Verify shopping cart badge is empty');
+    await expect(await productDetail.getShoppingCartEmpty()).toBe("");
+
+
+
 })
