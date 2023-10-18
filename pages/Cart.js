2ad3e85c1d93e29dcdef76e7cb2c0bd0ea3bd48c
hvbinh <havanbinhk13t01@gmail.com>
2023-10-18 14:37:33 +0700

    update TC

--- pages/Cart.js
+++ pages/Cart.js
@@ -3,7 +3,12 @@ export class Cart{
     {
         this.page = page;
         this.checkout_button = page.locator("//button[@name='checkout']");
-this.shopping_cart_badge = page.locator("//span[@class='shopping_cart_badge']");
+        this.shopping_cart_badge = page.locator("//span[@class='shopping_cart_badge']");
+        this.product_title = page.locator("//div[contains(@class,'inventory_item_name')]");
+        this.product_description = page.locator("//div[@class='inventory_item_desc']");
+        this.product_price = page.locator("//div[@class='inventory_item_price']");
+        this.product_quantity = page.locator("//div[@class='cart_quantity']");
+        
     }
     async clickToCheckoutButton()
     {
@@ -16,5 +21,21 @@ this.shopping_cart_badge = page.locator("//span[@class='shopping_cart_badge']");
     async clickToShoppingCartButton()
     {
         this.shopping_cart_badge.click();
+    }
+    getProductTitle()
+    {
+        return this.product_title.innerText();
+    }
+    getProductDescription()
+    {
+        return this.product_description.innerText();
+    }
+    getProductPrice()
+    {
+        return this.product_price.innerText();
+    }
+    getProductQuantity()
+    {
+        return this.product_quantity.innerText();
     }
 }
