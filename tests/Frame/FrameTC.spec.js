9d782d58b663a0b61322ef2fc2a5e771c055e5ca
hvbinh <havanbinhk13t01@gmail.com>
2023-10-23 16:36:24 +0700

    Update FrameTC.spec.js

--- tests/Frame/FrameTC.spec.js
+++ tests/Frame/FrameTC.spec.js
@@ -13,24 +13,22 @@ test('Verify that frame works correctly', async ({page})=>{
     
     logger.info('Go to url')
     await page.goto("https://skills.kynaenglish.vn/");
-    //await page.waitForLoadState();
-    await page.waitForTimeout(5000);
+    await page.waitForTimeout(3000);
 
     logger.info("Switch to iframe")
     const iframeChat = await page.frameLocator("//iframe[@id='cs_chat_iframe']");
 
     logger.info("Click to chat bar");
-    //await iframeChat.locator("//div[@class='border_overlay meshim_widget_widgets_BorderOverlay']").isEnabled();
     const chatBar = await iframeChat.locator("//div[@class='border_overlay meshim_widget_widgets_BorderOverlay']");
     await chatBar.isVisible();
     await chatBar.click();
     await iframeChat.locator("//input[@ng-model='login.username']").fill("user test");
+    await expect(await iframeChat.locator("//div[@class='ng-binding']").innerText()).toBe("Xin thông báo thời gian hỗ trợ mới của Kyna.vn bắt đầu từ 9h00-18h00, thứ 2 đến thứ 6 hàng tuần. Quý khách vui lòng để lại số điện thoại kèm lời nhắn, chúng tôi sẽ liên hệ trong thời gian sớm nhất. Xin cảm ơn.");
 
     await page.mainFrame();
-    await page.waitForTimeout(6000);
+    await expect(await page.locator("(//div[@class='title']/parent::div[@class='parent-category'])[1]").innerText()).toBe("Tất cả khóa học");
+    await page.waitForTimeout(3000);
 
 
-    
-    
 
 })
