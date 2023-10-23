0008d73a1172ffa87d37fabb3015b641eb21ced0
hvbinh <havanbinhk13t01@gmail.com>
2023-10-23 16:20:10 +0700

    Update FrameTC.spec.js

--- tests/Frame/FrameTC.spec.js
+++ tests/Frame/FrameTC.spec.js
@@ -3,29 +3,31 @@ import { logger } from '../../common/WinstonLog';
 
 let browser, context, page;
 
-test.beforeAll(async()=>{
-    browser = await chromium.launch();
-    context = await browser.newContext();
-    page = await context.newPage();
-})
+// test.beforeAll(async()=>{
+//     browser = await chromium.launch();
+//     context = await browser.newContext();
+//     page = await context.newPage();
+// })
 
-test('Verify that frame works correctly', async ()=>{
+test('Verify that frame works correctly', async ({page})=>{
     
     logger.info('Go to url')
     await page.goto("https://skills.kynaenglish.vn/");
+    //await page.waitForLoadState();
+    await page.waitForTimeout(5000);
 
-    
     logger.info("Switch to iframe")
     const iframeChat = await page.frameLocator("//iframe[@id='cs_chat_iframe']");
 
     logger.info("Click to chat bar");
     //await iframeChat.locator("//div[@class='border_overlay meshim_widget_widgets_BorderOverlay']").isEnabled();
     const chatBar = await iframeChat.locator("//div[@class='border_overlay meshim_widget_widgets_BorderOverlay']");
-    chatBar.click();
+    await chatBar.isVisible();
+    await chatBar.click();
     await iframeChat.locator("//input[@ng-model='login.username']").fill("user test");
 
     await page.mainFrame();
-    //await page.waitForTimeout(6000);
+    await page.waitForTimeout(6000);
 
 
     
